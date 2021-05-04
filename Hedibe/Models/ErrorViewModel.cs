fbd7bc2cd8d3a8072f3af1455a28510bb239694f
Niria <narutow21@interia.pl>
2021-05-04 08:28:28 +0200

    Add project files.

---
+++ Hedibe/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hedibe.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
